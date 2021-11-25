aa5be70cd3b931d88c7cbe4c74b6bbb4686867c9
unknown <43017102807>
2021-11-25 17:21:38 -0300

    Banco de Dados - DML

---
+++ BD/Scripts/senai_roman_02_DML.sql
@@ -0,0 +1,23 @@
+USE ROMAN_GEN
+GO
+
+INSERT INTO TEMA (titulo)
+VALUES	('Gestão'),('HQs'),('Linguagens de programação')
+GO
+
+INSERT INTO TIPOUSUARIO (titulo)
+VALUES ('PROFESSOR'),('ADMINISTRADOR')
+GO
+
+INSERT INTO USUARIO(idTipoUsuario,nome,email,senha)
+VALUES	(1,'Saulo','saulo@gmail.com','saulo123'),
+		(1,'Lucas','lucas@gmail.com','lucas123'),
+		(1,'Odirlei','odirlei@gmail.com','odirlei123'),
+		(2,'ADM','adm@gmail.com','adm123')
+GO
+
+INSERT INTO PROJETO (idUsuario,idTema,titulo)
+VALUES	(1,1,'Controle de estoque'),
+		(2,2,'Listagem de Personagens'),
+		(1,3,'Gufi')
+GO
